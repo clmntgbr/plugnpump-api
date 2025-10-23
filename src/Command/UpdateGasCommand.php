04d404170641f665cd2d41ab3417e08f9bf4ea92
Clément <33451311+clmntgbr@users.noreply.github.com>
2025-10-23 19:57:38 +0200

    Update Elasticsearch configuration and add UpdateGasCommand
    - Modified .env.dist to update Elasticsearch URL and user credentials.
    - Introduced UpdateGasCommand for updating gas prices at stations.

---
+++ src/Command/UpdateGasCommand.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+#[AsCommand(
+    name: 'update:gas',
+    description: 'Update the gas prices for the stations',
+    aliases: ['update:gas', 'gas:update'],
+)]
+class UpdateGasCommand extends Command
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        return Command::SUCCESS;
+    }
+}
